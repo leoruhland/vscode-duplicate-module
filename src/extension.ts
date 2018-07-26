a9211e1c5834f4958c00045512096b95c002042d
Leonardo Ruhland <leoruhland@gmail.com>
2018-07-26 14:51:39 -0300

    Add single file rename/replace feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,20 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as fs from 'fs-extra';
 import * as path from 'path';
-
 import escapeRegExp = require('lodash.escaperegexp');
 import * as vscode from 'vscode';
-
 import * as filepaths from './managers/filepaths';
 import * as promptUtils from './utils/prompt';
-
 import * as changeCase from 'change-case';
-
-
 import { IPluginSettings } from './types';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "duplicate-module" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
 
     let replaceInFile = async (someFile: string, replace: string, replaceWith: string) => {
         let data = await fs.readFile(someFile, 'utf8');
-
         data = data
             .split(replace)
             .join(replaceWith)
@@ -56,7 +41,7 @@ export function activate(context: vscode.ExtensionContext) {
             );
 
         return await fs.writeFile(someFile, data, 'utf8');
-    }
+    };
 
     let renameFiles = async (newPath: string, file: string, replace: string, replaceWith: string) => {
         const filePath = newPath + '/' + file;
@@ -91,7 +76,7 @@ export function activate(context: vscode.ExtensionContext) {
             return Promise.resolve();
         }
 
-    }
+    };
 
     let disposable = vscode.commands.registerCommand('extension.duplicateModule', async (uri: vscode.Uri, settings: IPluginSettings) => {
 
@@ -99,18 +84,22 @@ export function activate(context: vscode.ExtensionContext) {
         const oldPathParsed = path.parse(oldPath);
         const oldName = oldPathParsed.name;
         const oldPathStats = await fs.stat(oldPath);
-
         const newName = await promptUtils.name('');
-        const oldSearch = await promptUtils.srcName(oldName);
-        const newSearch = await promptUtils.destName(newName);
-
-        if (!newName) {
+        if (!newName || newName === '') {
             return;
         }
-        const newPath = filepaths.buildFilepath(oldPathParsed, oldPathStats, newName, settings);
+        const oldSearch = await promptUtils.srcName(oldName);
+        if (!oldSearch || oldSearch === '') {
+            return;
+        }
+        const newSearch = await promptUtils.destName(newName);
+        if (!newSearch || newSearch === '') {
+            return;
+        }
+
+        const newPath = filepaths.buildFilepath(oldPathParsed, oldPathStats, newName, { keepOriginalExtension: true, openFileAfterCopy: false });
         if (uri.fsPath === newPath) {
             vscode.window.showErrorMessage('You can\'t copy a file or directory on the same path.');
-
             return;
         }
         const newPathExists = await fs.pathExists(newPath);
@@ -120,42 +109,34 @@ export function activate(context: vscode.ExtensionContext) {
                 return;
             }
         }
-        try {
+
+        if (oldPathStats.isFile()) {
+            console.log(newPath);
+            console.log(settings);
+            console.log(oldPathParsed);
+            console.log(oldPathStats);
+            console.log(newName);
             await fs.copy(uri.fsPath, newPath);
-
-            /*
-            FS Version
-            await fs.readdir(newPath, async (err, files) => {
-                await files.forEach(async (file) => {
-                    const filePath = newPath + '/' + file;
+            await replaceInFile(newPath, oldSearch, newSearch);
+            vscode.window.showInformationMessage('Duplicate Module duplicated file with ' + oldSearch + ' renamed/replaced to ' + newSearch + ' at ' + newName);
+        } else {
+            try {
+                await fs.copy(uri.fsPath, newPath);
+                var readdirp = require('readdirp');
+                await readdirp({ root: newPath }).on('data', async (entry: any) => {
+                    const filePath = newPath + '/' + entry.path;
                     await replaceInFile(filePath, oldSearch, newSearch);
-
-                    await renameFiles(newPath, file, oldSearch, newSearch);
+                    await renameFiles(newPath, entry.path, oldSearch, newSearch);
                 });
-            })
-            */
-
-            var readdirp = require('readdirp');
-            await readdirp({ root: newPath }).on('data', async (entry: any) => {
-                const filePath = newPath + '/' + entry.path;
-                await replaceInFile(filePath, oldSearch, newSearch);
-                await renameFiles(newPath, entry.path, oldSearch, newSearch);
-            });
-
-
-            vscode.window.showInformationMessage('Duplicate Module duplicated ' + oldSearch + ' renamed/replaced to ' + newSearch + ' at ' + newName);
-
-            if (settings.openFileAfterCopy && oldPathStats.isFile()) {
-                return;
+                vscode.window.showInformationMessage('Duplicate Module duplicated folder with ' + oldSearch + ' renamed/replaced to ' + newSearch + ' at ' + newName);
+            } catch (err) {
+                const errMsgRegExp = new RegExp(escapeRegExp(oldPathParsed.dir), 'g');
+                const errMsg = err.message
+                    .replace(errMsgRegExp, '')
+                    .replace(/[\\|\/]/g, '')
+                    .replace(/`|'/g, '**');
+                vscode.window.showErrorMessage(`Error: ${errMsg}`);
             }
-        } catch (err) {
-            const errMsgRegExp = new RegExp(escapeRegExp(oldPathParsed.dir), 'g');
-            const errMsg = err.message
-                .replace(errMsgRegExp, '')
-                .replace(/[\\|\/]/g, '')
-                .replace(/`|'/g, '**');
-
-            vscode.window.showErrorMessage(`Error: ${errMsg}`);
         }
         return;
     });
--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -2,21 +2,21 @@ import * as vscode from 'vscode';
 
 export function name(filename: string): Promise<string> {
     return vscode.window.showInputBox({
-        placeHolder: 'Enter the new folter path for the duplicate.',
+        placeHolder: 'Enter the new name.',
         value: filename
     }) as Promise<string>;
 }
 
 export function srcName(filename: string): Promise<string> {
     return vscode.window.showInputBox({
-        placeHolder: 'Enter the original string to replace.',
+        placeHolder: 'Enter the source name.',
         value: filename
     }) as Promise<string>;
 }
 
 export function destName(filename: string): Promise<string> {
     return vscode.window.showInputBox({
-        placeHolder: 'Enter the new string to replace to.',
+        placeHolder: 'Enter the destination name.',
         value: filename
     }) as Promise<string>;
 }
@@ -27,6 +27,5 @@ export function overwrite(filepath: string): Promise<vscode.MessageItem | undefi
         title: 'OK',
         isCloseAffordance: false
     };
-
     return vscode.window.showWarningMessage(message, action) as Promise<vscode.MessageItem | undefined>;
 }
